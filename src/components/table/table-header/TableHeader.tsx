da551eb5ed2884b9dfa5c1567d4825949580995a
Ildar <ildar.1795@gmail.com>
2024-11-22 18:31:48 +0400

    feat: Создает компонент, который отображает шапку таблицы

---
+++ src/components/table/table-header/TableHeader.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+const TableHeader: FC = () => {
+	return (
+		<thead>
+			<tr>
+				<th></th>
+				<th>Название компании</th>
+				<th>Адрес</th>
+			</tr>
+		</thead>
+	)
+}
+
+export default TableHeader
