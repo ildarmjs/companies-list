cbf201a3a6a2371f953f00a776108fa90f2c07c1
Ildar <ildar.1795@gmail.com>
2024-11-22 19:37:34 +0400

    feat: Добавляет асинхронную загрузку компаний из API
    - Импортирует thunk `fetchCompanies` для загрузки данных о компаниях.
    - Добавляет состояние для отслеживания статуса загрузки и ошибок.
    - Реализует логику отображения состояния загрузки и ошибок.
    - Добавляет компоненты `Loading` и `ErrorMessage` для отображения статуса загрузки.

--- src/components/table/table-body/TableBody.tsx
+++ src/components/table/table-body/TableBody.tsx
@@ -1,18 +1,34 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import TableRow from '../table-row/TableRow'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../../store/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../../store/store'
 import ErrorBoundary from '../../error-boundary/ErrorBoundary'
 import NoCompaniesMessage from '../../no-companies-message/NoCompaniesMessage'
+import { fetchCompanies } from '../../../store/slices/companySlice'
+import Loading from '../../ui/loading/Loading'
+import ErrorMessage from '../../ui/eror-message/ErrorMessage'
 
 const TableBody: FC = () => {
-	const companies = useSelector((state: RootState) => state.companies.companies)
-
+	const dispatch: AppDispatch = useDispatch()
+	const { companies, status } = useSelector(
+		(state: RootState) => state.companies
+	)
+	console.log('companies:', companies)
 	const [editingNameId, setEditingNameId] = useState<number | null>(null)
 	const [editingAddressId, setEditingAddressId] = useState<number | null>(null)
 
 	const [editedName, setEditedName] = useState('')
 	const [editedAddress, setEditedAddress] = useState('')
+	useEffect(() => {
+		if (status === 'idle') {
+			dispatch(fetchCompanies())
+		}
+	}, [status, dispatch])
+
+	if (status === 'loading') return <Loading text='Загрузка...' />
+	if (status === 'failed')
+		return <ErrorMessage text='Ошибка при загрузке данных' />
+
 	const renderCompanies = () => {
 		if (companies.length === 0) {
 			return <NoCompaniesMessage />
