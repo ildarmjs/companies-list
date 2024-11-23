bad1835a2cf61f3d398d31ec653a3c2d98c352dd
Ildar <ildar.1795@gmail.com>
2024-11-23 19:52:06 +0400

    refactor: Интугрирует хуки useCompany, useEditableCompany, useInfinityScroll

--- src/components/table/table-body/TableBody.tsx
+++ src/components/table/table-body/TableBody.tsx
@@ -1,79 +1,30 @@
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import TableRow from '../table-row/TableRow'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../../store/store'
 import ErrorBoundary from '../../error-boundary/ErrorBoundary'
 import NoCompaniesMessage from '../../no-companies-message/NoCompaniesMessage'
-import { incrementPage, setCompanies } from '../../../store/slices/companySlice'
+import Loading from '../../ui/loading/Loading'
+import { useCompany } from '../../../hooks/useCompany'
+import { useInfinityScroll } from '../../../hooks/useInfinityScroll'
+import { useEditableCompany } from '../../../hooks/useEditableCompany'
 
 const TableBody: FC = () => {
-	const { companies, page } = useSelector((state: RootState) => state.companies)
-	const [editingNameId, setEditingNameId] = useState<number | null>(null)
-	const [editingAddressId, setEditingAddressId] = useState<number | null>(null)
-	const [fetching, setFetching] = useState(true)
-	const [editedName, setEditedName] = useState('')
-	const [editedAddress, setEditedAddress] = useState('')
-	const dispatch = useDispatch()
+	const { companies, loading, setFetching } = useCompany()
+	const editableCompany = useEditableCompany()
+	useInfinityScroll(() => setFetching(true))
 
-	useEffect(() => {
-		const fetchCompanies = async () => {
-			if (fetching) {
-				try {
-					const response = await fetch(
-						`https://dd6b73fe40abba67.mokky.dev/companies?page=${page}`
-					)
-					const data = await response.json()
-					dispatch(setCompanies([...companies, ...data.items]))
-					dispatch(incrementPage())
-				} catch (error) {
-					console.error('Error fetching companies:', error)
-				} finally {
-					setFetching(false)
-				}
-			}
-		}
-
-		fetchCompanies()
-	}, [fetching])
-	useEffect(() => {
-		document.addEventListener('scroll', scrollHandler)
-		return () => {
-			document.removeEventListener('scroll', scrollHandler)
-		}
-	}, [])
-	const scrollHandler = (e: Event) => {
-		const target = e.target as Document
-		if (
-			target.documentElement.scrollHeight -
-				(target.documentElement.scrollTop + window.innerHeight) <
-			100
-		) {
-			setFetching(true)
-		}
-	}
 	const renderCompanies = () => {
 		if (companies.length === 0) {
 			return <NoCompaniesMessage />
 		}
 
 		return companies.map(company => (
-			<TableRow
-				key={company.id}
-				company={company}
-				editedAddress={editedAddress}
-				editedName={editedName}
-				editingAddressId={editingAddressId}
-				editingNameId={editingNameId}
-				setEditedAddress={setEditedAddress}
-				setEditedName={setEditedName}
-				setEditingAddressId={setEditingAddressId}
-				setEditingNameId={setEditingNameId}
-			/>
+			<TableRow key={company.id} company={company} {...editableCompany} />
 		))
 	}
 	return (
 		<tbody>
 			<ErrorBoundary>{renderCompanies()}</ErrorBoundary>
+			{loading && <Loading text='Загрузка...' />}
 		</tbody>
 	)
 }
